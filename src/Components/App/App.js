ca518b7268b6083f58a79c22ff4636f7c228ef47
John Adams <adamsjr8576@gmail.com>
2020-05-13 13:45:47 -0600

    Add component and sketch files

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Boom Boom POW</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sketches/Sketch1.js
@@ -0,0 +1,25 @@
+const sketch1 = p => {
+ p.setup = () => {
+   p.createCanvas(p.windowWidth, p.windowHeight);
+ }
+
+ p.windowResized = () => {
+   p.resizeCanvas(p.windowWidth, p.windowHeight);
+ }
+
+ p.draw = () => {
+   p.stroke('purple');
+   p.strokeWeight(3);
+   if (p.mouseIsPressed) {
+     p.fill(0);
+   } else {
+     p.fill(255);
+   }
+   p.ellipse(p.mouseX, p.mouseY, 80, 80);
+ }
+
+ p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
+   }
+}
+
+export default sketch1;
