f314ae8ac79a0ce0d4a59a39080217194df63314
John Adams <adamsjr8576@gmail.com>
2020-05-13 14:00:03 -0600

    Create and import sketches array

---
+++ src/Sketches/sketches.js
@@ -0,0 +1,10 @@
+import sketch1 from './Sketch1.js';
+import sketch2 from './Sketch2';
+import sketch3 from './Sketch3';
+import sketch4 from './Sketch4';
+import sketch5 from './Sketch5';
+import sketch6 from './Sketch6';
+import sketch7 from './Sketch7';
+import sketch8 from './Sketch8';
+
+export const sketches = [sketch1, sketch2, sketch3, sketch4, sketch5, sketch6, sketch7, sketch8];
