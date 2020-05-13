d4ae98a3314c70e376c438985d351609d980220f
John Adams <adamsjr8576@gmail.com>
2020-05-13 13:53:48 -0600

    Create SketchesContainer skeleton

---
+++ src/Components/SketchesContainer/SketchesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SketchesContainer.scss';
+
+const SketchesContainer = () => {
+
+  return (
+    <section>
+    </section>
+  )
+}
+
+export default SketchesContainer;
